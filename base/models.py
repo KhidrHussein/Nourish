6e56ad6d79bf57c8498f23de3321a3bcafc11791
khidrhussein <husseinkhidr3@gmail.com>
2023-11-30 15:51:53 +0100

    theme installation

--- base/models.py
+++ base/models.py
@@ -1,3 +1,68 @@
 from django.db import models
 
 # Create your models here.
+
+# User Model
+class User(models.Model):
+    username = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=255)  # Use a hashed password, consider using Django's built-in authentication
+    full_name = models.CharField(max_length=255)
+    address = models.TextField()
+    phone_number = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.username
+    
+
+#Product Models
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity_available = models.IntegerField()
+    seller = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+    
+
+# Order Models
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    order_date = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=20)  # You might want to use choices for predefined statuses
+
+    def __str__(self):
+        return f"Order {self.id}"
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    price_at_purchase = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"OrderItem {self.id}"
+
+
+# Payment model
+
+class Payment(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    payment_date = models.DateTimeField(auto_now_add=True)
+    payment_method = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"Payment {self.id}"
--- base/views.py
+++ base/views.py
@@ -1,7 +1,61 @@
-from django.shortcuts import render
+from django.shortcuts import render ,redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm 
+
 
 # Create your views here.
 
 
 def home(request):
     return render(request, 'base/home.html')
+
+
+def loginPage(request):
+    page = 'login'
+
+    if request.user.is_authenticated:
+        return redirect('home')
+
+    if request.method == 'POST':
+        username = request.POST.get('username').lower()
+        password = request.POST.get('password')
+
+        try:
+           user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'User does not exist')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Username or password does not exist')
+
+
+    context = {'page': page}
+    return render(request, 'base/login_register.html', context)
+
+
+def registerPage(request):
+    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'An error occourred during registration!')
+
+    return render(request, 'base/login_register.html', {'form': form})
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect('home')
