52c725b17fee10db71aeaa2a39b3b07f812dd654
khidrhussein <husseinkhidr3@gmail.com>
2023-12-23 22:15:58 +0100

    added a requirements.txt file, serialized the remaining models

--- base/urls.py
+++ base/urls.py
@@ -1,11 +1,22 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from rest_framework.routers import DefaultRouter
+from .views import CategoryViewSet, OrderViewSet, OrderItemViewSet, ProductViewSet, PaymentViewSet
+
+
+router = DefaultRouter()
+router.register(r'category', CategoryViewSet, basename='category')
+router.register(r'order', OrderViewSet, basename='order')
+router.register(r'orderitem', OrderItemViewSet, basename='orderitem')
+router.register(r'product', ProductViewSet, basename='product')
+router.register(r'payment', PaymentViewSet, basename='payment')
+
 
 urlpatterns = [
+    path('api/base/', include(router.urls)),
     path('', views.home,  name="home"),
 
     path('login', views.log_in, name="login"),
-    # path('logout', views.logout, name="logout"),
     path('sign_up', views.sign_up, name="sign_up"),
     path('register', views.register, name="register"),
 ]
--- nourish/urls.py
+++ nourish/urls.py
@@ -6,7 +6,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('base.urls'))
+    path('', include('base.urls')),
+    path('', include('customusers.urls'))
+
 ]
 
 if settings.DEBUG:
